15a4d8b0f832af70d0c5207a3f538899a6fa8202
Roshan <roshan.sadanani+github@gmail.com>
2016-08-30 23:31:09 -0700

    fix token import from environment variable

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -3,7 +3,10 @@ try:
 except ImportError:
 	print "config.py does not exist"
 
-API_TOKEN = config.apitoken
+try:
+	API_TOKEN = config.apitoken
+except:
+	print "API_TOKEN cannot be retrieved from config.py"
 
 #DEFAULT_REPLY = "I support the following queries:\n"+"`top [position] [year]` - returns the top 5 players at a given position in a given year"
 
